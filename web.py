d48c6b071de640163317c65916398d1610727f21
Arunkumar Bhat <30899257+arunkumar1997@users.noreply.github.com>
2020-11-19 11:46:00 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,29 @@
+import requests, json
+# base URL
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+CITY = input("Please Enter the city name:")
+print("Locating city:",CITY,"....\n\n")
+API_KEY = "05cc201d696d928ab9b1aa615fbab3cf"
+# upadting the URL
+URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
+# HTTP request
+response = requests.get(URL)
+# checking the status code of the request
+if response.status_code == 200:
+   # getting data in the json format
+   data = response.json()
+   # getting the main dict block
+   main = data['main']
+   temperature = main['temp']
+   humidity = main['humidity']
+   pressure = main['pressure']
+   # weather report
+   report = data['weather']
+   print(f"{CITY:-^30}")
+   print(f"Temperature: {temperature}")
+   print(f"Humidity: {humidity}")
+   print(f"Pressure: {pressure}")
+   print(f"Weather Report: {report[0]['description']}")
+else:
+   # showing the error message
+   print("Error in the HTTP request or invalid city name:",CITY)
